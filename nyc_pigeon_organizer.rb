3c00fa006312e256dbbf61c08b70f3725fe09357
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-13 17:21:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
  output = data.each_with_object({}) do |(key,val), output|
    #binding.pry
     val.each do |inner_key,names|
@@ -15,9 +16,23 @@ def nyc_pigeon_organizer(data)
         output[name][key] = []
         end
         output[name][key].push(inner_key.to_s)
+=======
+  data.each_with_object({}) do |(key,val), output|
+    val.each do |inner_key,names|
+      binding.pry
+      names.each do |name|
+        if !output[name]
+        output[name] = {}
+        end
+        
+>>>>>>> 9b84e106c61397ed24c2f94bedcbcc018d711a9b
       end
     end
     output
   end
+<<<<<<< HEAD
    #binding.pry
+=======
+  binding.pry
+>>>>>>> 9b84e106c61397ed24c2f94bedcbcc018d711a9b
 end
